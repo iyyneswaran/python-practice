047435bafa4bb78290280b2634d360f27a9dfe37
iyyneswaran <pdee5660@gmail.com>
2025-11-14 18:43:21 +0530

    Fixed error in OccurenceSearch.py

--- OccurenceSearch.py
+++ OccurenceSearch.py
@@ -1,9 +1,8 @@
-string = str(input())
-a = str(input())
-b = str(input())
-search = a + b
-print(search)
+string = str(input().strip())
+a = str(input().strip())
+b = str(input().strip())
 count = 0
-if search in string:
-    count += 0
+for i in range(len(string)-1):
+    if string[i] == a and string[i+1] == b:
+        count += 1
 print(count)

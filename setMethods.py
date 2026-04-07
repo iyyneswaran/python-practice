0a6fa38545df8b9e3a84dad27cb36754be9536fa
iyyneswaran <pdee5660@gmail.com>
2026-04-07 21:18:36 +0530

    Added setMethods.py

---
+++ setMethods.py
@@ -0,0 +1,45 @@
+# various set methods 
+
+# add()
+def add_element(s, element):
+    return s.add(element)
+
+# clear()
+def clear_element(s):
+    return s.clear()
+
+# discard():
+def discard_element(s, element):
+    return s.discard(element)
+
+# remove(): 
+def remove_element(s, element):
+    return s.remove(element)
+
+# pop(): 
+def pop_element(s):
+    return s.pop()
+
+# len():
+def length(s):
+    return len(s)
+
+# sorted(): 
+def sort_set(a):
+    return sorted(a)
+
+# sum():
+def sum_setValues(a):
+    return sum(a)
+
+# union():
+def union_element(a, b):
+    return a.union(b)
+
+# difference():
+def diff(a, b):
+    return a.difference(b)
+
+# intersection():
+def common_in_set(a, b):
+    return a.intersection(b)

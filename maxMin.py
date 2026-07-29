b2e4503633d4ab4c6c3e096bd85a1bf582327f85
iyyneswaran <pdee5660@gmail.com>
2026-07-29 11:44:28 +0530

    Refractor: maxMin.py - minimum finding logic'

--- maxMin.py
+++ maxMin.py
@@ -1,15 +1,15 @@
 def maxMin(arr):
     maxValue = 0
-    minValue = 0
+    minValue = arr[0]
     result = []
-    for i in range(0, arr, 1):
+    for i in range(0, len(arr), 1):
         if arr[i] > maxValue:
             maxValue = arr[i]
-        if arr[i] > minValue and arr[i] < maxValue:
+        if arr[i] < minValue:
             minValue = arr[i]
     result.append(minValue)
     result.append(maxValue)
     return result
 
-arr = [4,6,7,8,3,5,34,54,64]
+arr = [4,6,7,8,2,3,5,34,54,64]
 print(maxMin(arr))
